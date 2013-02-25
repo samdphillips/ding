91504dfa64f37132456bcff34d7302b07ac18332
Sam Phillips <samdphillips@gmail.com>
2013-02-24 17:44:14 -0800

    Added simple identifiers

--- ding/read.py
+++ ding/read.py
@@ -1,4 +1,6 @@
 
+import re
+
 class Term:
     def is_eof_term(self):
         return False
@@ -9,6 +11,16 @@ class _EofTerm(Term):
 
 eof_term = _EofTerm()
 
+class IdTerm(Term):
+    def __init__(self, name):
+        self.name = name
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __repr__(self):
+        return '<IdTerm "{}">'.format(self.name)
+
 class ReaderError(Exception):
     def __init__(self, actual, expected):
         self.actual = actual
@@ -16,6 +28,20 @@ class ReaderError(Exception):
 
     def __str__(self):
         return "Expected {}, got {}".format(self.expected, self.actual)
+
+class Charset:
+    def __init__(self, rx):
+        self.rx = re.compile(rx)
+
+    def __contains__(self, c):
+        return self.rx.match(c) and True
+
+    def __add__(self, other):
+        return Charset('{}|{}'.format(self.rx.pattern, other.rx.pattern))
+
+digits = Charset('[0-9]')
+id_start_chars = Charset('[A-Za-z_]')
+id_chars = id_start_chars + digits
 
 class Reader:
     def __init__(self, io):
@@ -57,7 +83,6 @@ class Reader:
         self.io.read(2)
 
         if self.io.at_end():
-            ## XXX testme
             self.eof_error('comment end "*/"')
 
         while True:
@@ -75,11 +100,24 @@ class Reader:
                 self.eof_error('comment end "*/"')
 
 
+    def read_id_term(self):
+        s = ""
+
+        while self.io.peek(1) in id_chars:
+            s += self.io.read(1)
+
+        return IdTerm(s)
+
     def next_term(self):
         self.skip_space()
         
         if self.io.at_end():
             return eof_term
+        
+        c = self.io.peek(1)
+        
+        if c in id_start_chars:
+            return self.read_id_term()
 
 
 from io import StringIO
@@ -92,7 +130,11 @@ class TestReader(unittest.TestCase):
         io = ding.io.BufferedText(sio)
         return Reader(io)
 
-    def assert_next_eof(self, r):
+    def assert_term(self, r, term):
+        t = r.next_term()
+        self.assertEqual(t, term)
+
+    def assert_eof_term(self, r):
         t = r.next_term()
         self.assertTrue(t.is_eof_term())
 
@@ -106,31 +148,31 @@ class TestReader(unittest.TestCase):
 
     def test_read_empty(self):
         r = self.setup_reader('     ')
-        self.assert_next_eof(r)
+        self.assert_eof_term(r)
 
     def test_read_line_comment(self):
         r = self.setup_reader('  // this is a test \n')
-        self.assert_next_eof(r)
+        self.assert_eof_term(r)
 
     def test_read_line_comment_eof(self):
         r = self.setup_reader('  // this is a test')
-        self.assert_next_eof(r)
+        self.assert_eof_term(r)
 
     def test_read_block_comment_eof(self):
         r = self.setup_reader('  /* this is a test */  ')
-        self.assert_next_eof(r)
+        self.assert_eof_term(r)
 
     def test_read_block_comment_pad_eof(self):
         r = self.setup_reader('  /* this is a  test */  ')
-        self.assert_next_eof(r)
+        self.assert_eof_term(r)
 
     def test_read_block_comment_multiline(self):
         r = self.setup_reader('  /* this is a\n\n\ntest */  ')
-        self.assert_next_eof(r)
+        self.assert_eof_term(r)
 
     def test_read_block_comment_stars_eof(self):
         r = self.setup_reader('  /* this is a ***  test */  ')
-        self.assert_next_eof(r)
+        self.assert_eof_term(r)
 
     def test_read_block_comment_error1(self):
         r = self.setup_reader('  /*')
@@ -139,3 +181,28 @@ class TestReader(unittest.TestCase):
     def test_read_block_comment_error2(self):
         r = self.setup_reader('  /*  ')
         self.assert_next_error(r)
+
+    def test_read_id(self):
+        r = self.setup_reader(' a ')
+        self.assert_term(r, IdTerm('a'))
+        self.assert_eof_term(r)
+
+    def test_read_id_long(self):
+        r = self.setup_reader(' ab ab1 ab2 ')
+        self.assert_term(r, IdTerm('ab'))
+        self.assert_term(r, IdTerm('ab1'))
+        self.assert_term(r, IdTerm('ab2'))
+        self.assert_eof_term(r)
+
+    def test_read_3id(self):
+        r = self.setup_reader(' a b c')
+        self.assert_term(r, IdTerm('a'))
+        self.assert_term(r, IdTerm('b'))
+        self.assert_term(r, IdTerm('c'))
+        self.assert_eof_term(r)
+
+    def test_read_id_long_underscore(self):
+        r = self.setup_reader(' a_b_c1 ')
+        self.assert_term(r, IdTerm('a_b_c1'))
+        self.assert_eof_term(r)
+
