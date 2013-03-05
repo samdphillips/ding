7a31abe4bcf4d9ae614adb4d5d4284b28e3bfa69
Sam Phillips <samdphillips@gmail.com>
2013-03-05 15:15:20 -0800

    Ported reader so far

--- lib/ding/reader.rb
+++ lib/ding/reader.rb
@@ -1,7 +1,13 @@
 
+require 'singleton'
+
 module Ding
     class Charset
         attr_reader :pat
+
+        def self.from_chars(s)
+            self.new("[#{Regexp.escape(s)}]")
+        end
 
         def initialize(rx)
             @pat = Regexp.new(rx)
@@ -13,6 +19,142 @@ module Ding
 
         def +(other_cs)
             Charset.new(Regexp.union(@pat, other_cs.pat))
+        end
+    end
+
+    class Term
+        def eof_term?
+            false
+        end
+
+        def id_term?
+            false
+        end
+    end
+
+    class IdTerm < Term
+        attr_reader :name
+
+        def initialize(name)
+            @name = name
+        end
+
+        def id_term?
+            true
+        end
+    end
+
+    class EofTerm < Term
+        include Singleton
+        def eof_term?
+            true
+        end
+    end
+
+    class ReaderError < StandardError
+        def initialize(expected, actual)
+            @actual = actual
+            @expected = expected
+        end
+
+        def to_s
+            "ReaderError: expected #{@expected}, got: #{@actual.inspect}"
+        end
+    end
+
+    class Reader
+        def initialize(io)
+            @io = io
+        end
+
+        Space = Charset.new(/\s/)
+        Digit = Charset.new(/\d/)
+        IdStart = Charset.new(/[a-zA-Z_\+\-]/)
+        IdChar = IdStart + Digit
+
+        def skip_spaces
+            while true do
+                c = @io.peek(1)
+                if Space.contains?(c) then
+                    @io.read(1)
+                elsif c == '/' then
+                    s = @io.peek(2)
+                    if s == '//' then
+                        skip_line_comment
+                    elsif s == '/*' then
+                        skip_block_comment
+                    else
+                        break
+                    end
+                else
+                    break
+                end
+            end
+        end
+
+        def skip_line_comment
+            @io.read(2)
+
+            unless @io.at_end? then
+                while true do
+                    if @io.at_end? then
+                        break
+                    else
+                        c = @io.peek(1)
+                        if c == "" or c == "\n" then
+                            @io.read(1)
+                            break
+                        end
+                        @io.read(1)
+                        c = @io.peek(1)
+                    end
+                end
+            end
+        end
+
+        def skip_block_comment
+            @io.read(2)
+
+            while true do
+                if @io.at_end? then
+                    raise ReaderError.new('*/', '<eof>')
+                end
+
+                s = @io.peek(2)
+
+                if s == '*/' then
+                    @io.read(2)
+                    break
+                elsif s[1] == '*' then
+                    @io.read(1)
+                else
+                    @io.read(2)
+                end
+            end
+        end
+
+        def read_id_term
+            s = ""
+            while IdChar.contains?(@io.peek(1)) do
+                s << @io.read(1)
+            end
+            IdTerm.new(s)
+        end
+
+        def next_term
+            skip_spaces
+
+            if @io.at_end? then
+                return EofTerm.instance
+            end
+
+            c = @io.peek(1)
+
+            if IdStart.contains?(c) then
+                read_id_term
+            else
+                raise ReaderError.new('term', c)
+            end
         end
     end
 end
---
+++ spec/reader_spec.rb
@@ -0,0 +1,114 @@
+
+require 'ding'
+
+require 'stringio'
+
+def setup_reader(s)
+    Ding::Reader.new(Ding::BufIo.new(StringIO.new(s)))
+end
+
+class ReadExpectation
+    def initialize(type, value, &matcher)
+        @type = type
+        @value = value
+        @matcher = matcher
+    end
+
+    def matches?(reader)
+        @term = reader.next_term
+        @matcher.call(@term)
+    end
+
+    def failure_message
+        "expected #{@type}[#{@value}], got #{@term.inspect}"
+    end
+
+    def negative_failure_message
+        "expected #{@term.inspect} to not be #{@type}[#{@value}]"
+    end
+end
+
+def read_id(name)
+    ReadExpectation.new('IdTerm', name) do | term |
+        term.id_term? and term.name == name
+    end
+end
+
+def read_eof
+    ReadExpectation.new('EofTerm', nil) do | term |
+        term.eof_term?
+    end
+end
+
+describe Ding::Reader do
+    it "should skip whitespace" do
+        r = setup_reader('    ')
+        r.should read_eof
+    end
+
+    it "should skip line comments" do
+        r = setup_reader("  // this is a test \n")
+        r.should read_eof
+    end
+
+    it "should skip line comments at the end of file" do
+        r = setup_reader('  // this is a test')
+        r.should read_eof
+    end
+
+    it "should skip block comments" do
+        r = setup_reader('  /* this is a test */  ')
+        r.should read_eof
+    end
+
+    it "should skip multiline block comments" do
+        r = setup_reader("  /* this is a \n\n\n    test */   ")
+        r.should read_eof
+    end
+
+    it "should skip a block comment with some stars in it" do
+        r = setup_reader('  /* this is a ***  test */  ')
+        r.should read_eof
+    end
+
+    it "should raise an error if a block comment is not closed" do
+        r = setup_reader('   /* this is a ')
+        lambda { r.next_term }.should raise_error(Ding::ReaderError)
+    end
+
+    it "should read a short id '  a  '" do
+        r = setup_reader('  a  ')
+        r.should read_id('a')
+        r.should read_eof
+    end
+
+    it "should read a long id ' abc123_456 '" do
+        r = setup_reader(' abc123_456 ')
+        r.should read_id('abc123_456')
+        r.should read_eof
+    end
+
+    it "should read a series of ids ' a b c '" do
+        r = setup_reader(' a b c ')
+        r.should read_id('a')
+        r.should read_id('b')
+        r.should read_id('c')
+        r.should read_eof
+    end
+
+    it "should read a series of ids ' a + b '" do
+        r = setup_reader(' a + b ')
+        r.should read_id('a')
+        r.should read_id('+')
+        r.should read_id('b')
+        r.should read_eof
+    end
+
+    it "should read a single id ' a+b '" do
+        r = setup_reader(' a+b ')
+        r.should read_id('a+b')
+        r.should read_eof
+    end
+
+end
+
